71d17625c74e358c926b1d8f549b7a9296c43b97
elcid00 <elcid00@aol.com>
2019-03-06 15:34:05 -0500

    fixed final project

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
  
+
+
+  
 // creates random number of the score to match
     var scoreToMatch = Math.floor(Math.random()*102+19)
 
@@ -7,21 +10,41 @@ $(document).ready(function() {
     $("#scoreToMatch").text(scoreToMatch);
 
 // creates random variable for each gem at the start of the game
-    var valCrystal1 = Math.floor(Math.random() * 12 + 1)
-    var valCrystal2 = Math.floor(Math.random() * 12 + 1)
-    var valCrystal3 = Math.floor(Math.random() * 12 + 1)
-    var valCrystal4 = Math.floor(Math.random() * 12 + 1)
+  var valCrystal1;
+  var valCrystal2;
+  var valCrystal3;
+  var valCrystal4;
+    // var valCrystal1 = Math.floor(Math.random() * 12 + 1)
+    // var valCrystal2 = Math.floor(Math.random() * 12 + 1)
+    // var valCrystal3 = Math.floor(Math.random() * 12 + 1)
+    // var valCrystal4 = Math.floor(Math.random() * 12 + 1)
     
 // establishes initial value for the variables below
     var totalScore = 0; 
     var wins = 0;
     var losses = 0;
+
+//reset function called to initialize value for crystals
+  reset();
+
+  // upon completion of game with win or loss, code resetting game -- keeping wins and losses tallied
+  function reset(){
+    scoreToMatch = Math.floor(Math.random() * 101 + 19);
+      console.log(scoreToMatch)
+      $('#scoreToMatch').text(scoreToMatch);
+      valCrystal1 = Math.floor(Math.random() * 12 + 1)
+      valCrystal2 = Math.floor(Math.random() * 12 + 1)
+      valCrystal3 = Math.floor(Math.random() * 12 + 1)
+      valCrystal4 = Math.floor(Math.random() * 12 + 1)
+      totalScore= 0;
+      $('#totalScore').text(totalScore); 
+      } 
     
 // script for when red gem is clicked
     $('#red').on('click', function(){
       totalScore = totalScore + valCrystal1;
       console.log("New Score= " + totalScore);
-      $('#totalScore').text("Total Score: " + totalScore); 
+      $('#totalScore').text(totalScore); 
           if (totalScore == scoreToMatch){
             winner();
           }
@@ -34,7 +57,7 @@ $(document).ready(function() {
     $('#blue').on('click', function(){
       totalScore = totalScore + valCrystal2;
       console.log("New totalScore= " + totalScore);
-      $('#totalScore').text("Total Score: " + totalScore);  
+      $('#totalScore').text(totalScore);  
           if (totalScore == scoreToMatch){
             winner();
           }
@@ -47,7 +70,7 @@ $(document).ready(function() {
     $('#orange').on('click', function(){
       totalScore = totalScore + valCrystal3;
       console.log("New totalScore= " + totalScore);
-      $('#totalScore').text("Total Score: " + totalScore); 
+      $('#totalScore').text(totalScore); 
   
             if (totalScore == scoreToMatch){
             winner();
@@ -61,7 +84,7 @@ $(document).ready(function() {
     $('#green').on('click', function(){
       totalScore = totalScore + valCrystal4;
       console.log("New totalScore= " + totalScore);
-      $('#totalScore').text("Total Score: " + totalScore); 
+      $('#totalScore').text(totalScore); 
         
         if (totalScore == scoreToMatch){
         winner();
@@ -89,17 +112,6 @@ $(document).ready(function() {
             reset()
         }
 
-// upon completion of game with win or loss, code resetting game -- keeping wins and losses tallied
-    function reset(){
-        scoreToMatch = Math.floor(Math.random() * 101 + 19);
-          console.log(scoreToMatch)
-          $('#scoreToMatch').text("Score to Match: " + scoreToMatch);
-          var valCrystal1 = Math.floor(Math.random() * 12 + 1)
-          var valCrystal2 = Math.floor(Math.random() * 12 + 1)
-          var valCrystal3 = Math.floor(Math.random() * 12 + 1)
-          var valCrystal4 = Math.floor(Math.random() * 12 + 1)
-          totalScore= 0;
-          $('#totalScore').text("Total Score: " + totalScore); 
-          } 
+
 
 });
